aa821019f75966941b78ab49d3fb8f6945f76f63
熊新新 <820046719@qq.com>
2020-10-26 20:41:27 +0800

    初始化项目

---
+++ src/main/java/com/xxx/studyamazons3/StudyAmazons3Application.java
@@ -0,0 +1,13 @@
+package com.xxx.studyamazons3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAmazons3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyAmazons3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/xxx/studyamazons3/StudyAmazons3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxx.studyamazons3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAmazons3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
