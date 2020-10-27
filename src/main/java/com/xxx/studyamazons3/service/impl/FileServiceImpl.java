72a1040e069ebf4dbece5afeddcfcc7a6640d921
熊新新 <820046719@qq.com>
2020-10-27 22:08:21 +0800

    新增文æ文件上传逻辑

--- src/main/java/com/xxx/studyamazons3/service/impl/FileServiceImpl.java
+++ src/main/java/com/xxx/studyamazons3/service/impl/FileServiceImpl.java
@@ -1,9 +1,17 @@
 package com.xxx.studyamazons3.service.impl;
 
 import com.xxx.studyamazons3.service.FileService;
+import com.xxx.studyamazons3.util.BucketConstant;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.errors.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class FileServiceImpl implements FileService {
 
@@ -17,5 +25,35 @@ public class FileServiceImpl implements FileService {
      */
     @Override
     public void uploadFile(MultipartFile targetFile) {
+        try {
+            boolean bucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(BucketConstant.IMAGE_NAME).build());
+            if(!bucketExists){
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(BucketConstant.IMAGE_NAME).region("us-west-1").build());
+            }
+
+            minioClient.putObject()
+        } catch (ErrorResponseException e) {
+            e.printStackTrace();
+        } catch (InsufficientDataException e) {
+            e.printStackTrace();
+        } catch (InternalException e) {
+            e.printStackTrace();
+        } catch (InvalidBucketNameException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidResponseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (XmlParserException e) {
+            e.printStackTrace();
+        } catch (RegionConflictException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/xxx/studyamazons3/util/BucketConstant.java
@@ -0,0 +1,5 @@
+package com.xxx.studyamazons3.util;
+
+public class BucketConstant {
+    public static final String IMAGE_NAME = "image_name";
+}
