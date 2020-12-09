e61d91fa5813d96ca30c95d60424f754766f6fc8
xiongxinxin <xxxionghenu@163.com>
2020-12-10 00:26:04 +0800

    文件下载

---
+++ src/main/java/com/xxx/studyamazons3/controller/FileDownloadController.java
@@ -0,0 +1,7 @@
+package com.xxx.studyamazons3.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileDownloadController {
+}
---
+++ src/main/java/com/xxx/studyamazons3/service/FileDownloadService.java
@@ -0,0 +1,4 @@
+package com.xxx.studyamazons3.service;
+
+public interface FileDownloadService {
+}
